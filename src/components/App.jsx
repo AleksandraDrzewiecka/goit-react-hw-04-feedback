2f099aa7ff1db7669bf65d4e8f2c98d7b7f6ed2d
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-04-19 18:07:51 +0200

    hooki

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
+import { Section } from './Section/Section.jsx';
+import { Notification } from './Notifications/Notification.jsx';
+
+export const countTotalFeedback = (good, neutral, bad) => {
+  return good + neutral + bad;
+};
+export const countPositiveFeedbackPercentage = (good, totalFeedback) => {
+  return (Math.round((good / totalFeedback) * 10000) / 100).toFixed(2);
+};
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={{ good: 'Good', neutral: 'Neutral', bad: 'Bad' }}
+          onLeaveFeedback={{
+            handleGood: () => setGood(good + 1),
+            handleNeutral: () => setNeutral(neutral + 1),
+            handleBad: () => setBad(bad + 1),
+          }}
+        ></FeedbackOptions>
+
+        <Statistics
+          title="Statistics"
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback(good, neutral, bad)}
+          positivePercentage={countPositiveFeedbackPercentage(
+            good,
+            countTotalFeedback(good, neutral, bad)
+          )}
+        >
+          <Notification
+            isFeedback={countTotalFeedback(good, neutral, bad)}
+            message="There is no feedback"
+          ></Notification>
+        </Statistics>
+      </Section>
+    </>
   );
 };
+
